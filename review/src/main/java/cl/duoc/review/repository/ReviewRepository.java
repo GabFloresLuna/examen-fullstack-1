332f3f650afd64b7c9804193387dc625ea99c717
Antil <gab.floresl@duocuc.cl>
2026-06-30 11:54:15 -0400

    Cambio en el argumento del metodo findbydestinationid de Long -> UUID

--- review/src/main/java/cl/duoc/review/repository/ReviewRepository.java
+++ review/src/main/java/cl/duoc/review/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package cl.duoc.review.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import cl.duoc.review.model.Review;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByDestinationId(Long destinationId);
+    List<Review> findByDestinationId(UUID destinationId);
 }
