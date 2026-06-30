1e33fa50f368a293d25fe0a99c1c48e10ef870a9
Antil <gab.floresl@duocuc.cl>
2026-06-30 11:25:48 -0400

    Script SQL Creado en migration

---
+++ review/src/main/resources/db/migration/V1__create_review_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE reviews (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    destination_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    comment VARCHAR(500),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
